f49e418306eefa5a39cfb930e73ee7646e39e389
Seito Taka <amanoese@gmail.com>
2019-01-27 00:17:38 +0900

    update cariburate

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const express = require("express");
 const app = express()
 const robot = require("robotjs");
 
-app.get('/', (req, res) => {
-  let {x,y} = req.query
+app.get('/:x/:y', (req, res) => {
+  let {x,y} = req.params
   if(x == null || y == null) {return}
 
-  robot.moveMouse(x, y);
+//  let x_zoom = x * 1920 / 300
+//  let y_zoom = y * 1080 / 200
+  let zoom = 768  / 1000
+  let x_zoom = x * zoom
+  let y_zoom = y * zoom
+  robot.moveMouse(x_zoom, y_zoom);
   robot.mouseClick();
   console.log('click',{x,y})
   res.send(`click x:${x} y:${y}\n`)
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
